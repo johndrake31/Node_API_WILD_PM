f7d29fbec35e05b3ac44fdff8ebd93058be84368
Peter May <peter.may@winzana.com>
2022-03-08 11:57:22 +0100

    name property required:true

--- models/Wilder.js
+++ models/Wilder.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 const WilderSchema = new Schema({
-  name: {type: String, unique: true},
+  name: {type: String, unique: true, required: true},
   city: String,
   skills: [{ title: String, votes: Number }],
 });
